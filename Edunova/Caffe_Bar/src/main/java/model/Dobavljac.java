5c77501ef575a3053f065b41dd3bad2c2bbdc172
markohonda1 <51539769+markohonda1@users.noreply.github.com>
2020-03-09 16:24:53 +0100

    Hibernate on my project

---
+++ Edunova/Caffe_Bar/src/main/java/model/Dobavljac.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Grbeš
+ */
+@Entity
+public class Dobavljac {
+    private String naziv;
+    private String adresa;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+    
+}
---
+++ Edunova/Caffe_Bar/src/main/java/model/Entitet.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author Grbeš
+ */
+@MappedSuperclass
+public abstract class Entitet {
+    private Integer sifra;
+
+    public Integer getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(Integer sifra) {
+        this.sifra = sifra;
+    }
+    
+}
